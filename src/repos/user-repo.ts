33484601d789cce165a2f55ba7d51a491b9e555d
wandeltk <wandeltk@gmail.com>
2020-05-11 14:53:15 -0600

    get all and get by id functions in repo

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -1,18 +1,36 @@
 import { User } from '../models/user';
 import { CrudRepository } from './crud-repo';
+import { PoolClient } from 'pg';
+import { connectionPool } from '..';
 
 
 
 export class UserRepo implements CrudRepository<User> {
-    getAll(): Promise<User[]>{
+    
+    /**
+     * Gets all users from data base
+     */
+    async getAll(): Promise<User[]>{
+        let client: PoolClient;
+        let sql = 'select * from ers_users';
+        let rs = await client.query(sql);
+        
+        return rs.rows;
+    }
+
+    async getById(id: number): Promise<User>{
+        let client: PoolClient;
+        let sql = 'select * from ers_users where id = $1'
+        let rs = await client.query(sql, [id]);
+
+        return rs.rows[0];
 
     }
-    getById(id: number): Promise<User>{
 
-    }
     save(newUser: User): Promise<User>{
 
     }
+
     deleteById(id: number): Promise<boolean>{
 
     }
