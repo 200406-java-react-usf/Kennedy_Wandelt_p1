53bade993672f87b31383dbc41b3175cb8fdb166
wandeltk <wandeltk@gmail.com>
2020-05-11 15:17:03 -0600

    save method for users

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -12,6 +12,7 @@ export class UserRepo implements CrudRepository<User> {
      */
     async getAll(): Promise<User[]>{
         let client: PoolClient;
+        client = await connectionPool.connect();
         let sql = 'select * from ers_users';
         let rs = await client.query(sql);
 
@@ -24,20 +25,24 @@ export class UserRepo implements CrudRepository<User> {
      */
     async getById(id: number): Promise<User>{
         let client: PoolClient;
+        client = await connectionPool.connect();
         let sql = 'select * from ers_users where id = $1'
         let rs = await client.query(sql, [id]);
 
         return rs.rows[0];
-
     }
-    /*
 
-    save(newUser: User): Promise<User>{
+    async save(newUser: User): Promise<User>{
+        let client: PoolClient;
+        client = await connectionPool.connect();
+        let sql = 'insert into ers_users(ers_user_id, username, password, first_name, last_name, email, user_role_id) values ($1, $2, $3, $4, $5, $6, $7)';
+        let rs = await client.query(sql, [newUser.id, newUser.un, newUser.pass, newUser.fn, newUser.ln, newUser.email, newUser.role]);
 
+        return rs.rows[0];
     }
 
     deleteById(id: number): Promise<boolean>{
 
     }
-    */
+    
 }
