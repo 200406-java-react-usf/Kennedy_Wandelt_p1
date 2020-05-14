12a3daf3ca13585620eb3dc6ecbbf531167ab429
wandeltk <wandeltk@gmail.com>
2020-05-13 19:08:52 -0600

    init fm guard

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -11,3 +11,18 @@ const adminGuard = (req: Request, resp: Response, next) => {
         resp.status(403).json(new AuthorizationError());
     }
 }
+
+const fmGuard = (req: Request, resp: Response, next) => {
+    if (!req.session.principal) {
+        resp.status(401).json(new AuthenticationError('No session found, please login.'));
+    } else if (req.session.principal.role === 'fmanager') {
+        next();
+    } else {
+        resp.status(403).json(new AuthorizationError());
+    }
+}
+
+export {
+    adminGuard,
+    fmGuard
+}
