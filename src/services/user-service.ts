3f51c0b625322c841c75dd7e8d793625b19fe647
wandeltk <wandeltk@gmail.com>
2020-05-13 18:51:27 -0600

    added conflict validation to save method

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,6 +1,12 @@
 import {User} from '../models/user';
 import {UserRepo} from '../repos/user-repo';
-import {ResourceNotFoundError} from '../error/error';
+import {isEmptyObject,
+        isValidObject,
+        isValidString} from '../util/validation';
+import {ResourceNotFoundError,
+        BadRequestError,
+        DataPersistanceError,
+        InternalServerError} from '../error/error';
 
 export class UserService {
     constructor(private userRepo: UserRepo) {
@@ -19,10 +25,32 @@ export class UserService {
     async getUserById(id: number): Promise<User> {
         let user = await this.userRepo.getById(id);
 
+        if(isEmptyObject(user)) {
+            throw new ResourceNotFoundError('A user could not be found given the input id.');
+        }
         return user;
     }
 
     async addNewUser(newUser: User): Promise<User> {
+
+        if(!isValidObject(newUser, 'id')) {
+            throw new BadRequestError('User object given was invalid.');
+        }
+        
+        let conflict = {};
+
+        conflict = await this.userRepo.getUserByUniqueKey('email', newUser.email);
+
+        if(!isEmptyObject(conflict)){
+            throw new DataPersistanceError('A user with this email already exists.')
+        }
+
+        conflict = await this.userRepo.getUserByUniqueKey('username', newUser.un);
+
+        if(!isEmptyObject(conflict)){
+            throw new DataPersistanceError('A user with this username already exists.')
+        }
+
         let user = await this.userRepo.save(newUser);
 
         return user;
@@ -37,6 +65,10 @@ export class UserService {
     async authUser(username: string, password: string): Promise<User>{
         let authUser: User;
         
+        if(!isValidString(username) || !isValidString(password)){
+            throw new BadRequestError('Given username and/or password are not valid strings.');
+        }
+
         authUser = await this.userRepo.getUserByCreds(username, password);
 
         return (authUser);
