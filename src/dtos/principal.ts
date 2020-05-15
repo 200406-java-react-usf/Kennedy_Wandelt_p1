3911730a459b4023e4fac19bb2151e2f3aa4e227
wandeltk <wandeltk@gmail.com>
2020-05-14 21:24:46 -0600

    chnged principal model

--- src/dtos/principal.ts
+++ src/dtos/principal.ts
@@ -1,12 +1,12 @@
 export class Principal {
 
-    id: number;
+    ers_user_id: number;
     username: string;
     role_name: string;
 
-    constructor(id: number, un: string, role_name: string) {
-        this.id = id;
-        this.username = un;
+    constructor(ers_user_id: number, username: string, role_name: string) {
+        this.ers_user_id = ers_user_id;
+        this.username = username;
         this.role_name = role_name;
     }
     
